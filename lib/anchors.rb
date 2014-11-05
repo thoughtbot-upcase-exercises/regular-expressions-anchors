c2e12785bb063d00821dfd5865b172862eba293a
Gabe Berke-Williams <gabebw@gabebw.com>
2014-11-05 15:56:44 -0500

    Make tests more stringent; use a class

--- lib/anchors.rb
+++ lib/anchors.rb
@@ -1,34 +1,35 @@
 # Implement the below functions in order.
-module Anchors
+class Anchors
   # Returns a regex that matches a string that has at least one line that has
   # exactly 1 word in it. It may have other lines that don't match this regex.
-  def regex_with_at_least_one_line_that_has_exactly_one_word_in_it
+  def matching_at_least_one_line_that_has_exactly_one_word_in_it
     /WRITE ME/
   end
 
   # Returns a regex that matches a string that starts with "hello".
-  def regex_matching_a_string_starting_with_hello
+  def matching_a_string_starting_with_hello
     /WRITE ME/
   end
 
-  # Returns a regex that matches a string that starts with "goodbye".
-  def regex_matching_a_string_ending_with_goodbye
+  # Returns a regex that matches a string that ends with "goodbye".
+  def ends_with_goodbye
     /WRITE ME/
   end
 
-  # Returns a regex that matches "greeting" exactly.
-  def regex_matching_greeting_exactly
+  # Returns a regex that matches the word "greeting" exactly. It should not
+  # match any other words.
+  def matching_greeting_exactly
     /WRITE ME/
   end
 
   # Returns a regex that matches exactly one word.
-  def regex_matching_exactly_one_word
+  def matching_exactly_one_word
     /WRITE ME/
   end
 
   # Returns a regex that has the word "and" somewhere in it. This should match
   # "and", but not "demand", for example.
-  def regex_matching_the_word_and
+  def matching_the_word_and
     /WRITE ME/
   end
 end
--- spec/anchors_spec.rb
+++ spec/anchors_spec.rb
@@ -1,97 +1,109 @@
 require "spec_helper"
 
-describe "regex_with_at_least_one_line_that_has_exactly_one_word_in_it" do
-  it "matches 'one\ntwo words'" do
-    expect(regex_with_at_least_one_line_that_has_exactly_one_word_in_it).to match("one\ntwo words")
+describe "matching_at_least_one_line_that_has_exactly_one_word_in_it" do
+  it 'matches "one\ntwo words"' do
+    expect(Anchors.new.matching_at_least_one_line_that_has_exactly_one_word_in_it).to match("one\ntwo words")
+  end
+
+  it "matches 'word'" do
+    expect(Anchors.new.matching_at_least_one_line_that_has_exactly_one_word_in_it).to match("word")
   end
 
   it "does not match 'A1 Sauce'" do
-    expect(regex_with_at_least_one_line_that_has_exactly_one_word_in_it).not_to match("A1 Sauce")
+    expect(Anchors.new.matching_at_least_one_line_that_has_exactly_one_word_in_it).not_to match("A1 Sauce")
   end
 
   it "does not match the empty string" do
-    expect(regex_with_at_least_one_line_that_has_exactly_one_word_in_it).not_to match("")
+    expect(Anchors.new.matching_at_least_one_line_that_has_exactly_one_word_in_it).not_to match("")
   end
 end
 
-describe "regex_matching_a_string_starting_with_hello" do
+describe "matching_a_string_starting_with_hello" do
   it "matches 'hello'" do
-    expect(regex_matching_a_string_starting_with_hello).to match "hello"
+    expect(Anchors.new.matching_a_string_starting_with_hello).to match "hello"
   end
 
   it "matches 'hello there'" do
-    expect(regex_matching_a_string_starting_with_hello).to match "hello there"
+    expect(Anchors.new.matching_a_string_starting_with_hello).to match "hello there"
   end
 
   it "does not match 'oh hello'" do
-    expect(regex_matching_a_string_starting_with_hello).not_to match "oh hello"
+    expect(Anchors.new.matching_a_string_starting_with_hello).not_to match "oh hello"
   end
 
-  it "does not match 'oh\nhello'" do
-    expect(regex_matching_a_string_starting_with_hello).not_to match "oh\nhello"
+  it "does not match 'hellion'" do
+    expect(Anchors.new.matching_a_string_starting_with_hello).not_to match "hellion"
+  end
+
+  it 'does not match "oh\nhello"' do
+    expect(Anchors.new.matching_a_string_starting_with_hello).not_to match "oh\nhello"
   end
 end
 
-describe "regex_matching_a_string_ending_with_goodbye" do
+describe "ends_with_goodbye" do
   it "matches 'goodbye'" do
-    expect(regex_matching_a_string_ending_with_goodbye).to match "goodbye"
+    expect(Anchors.new.ends_with_goodbye).to match "goodbye"
   end
 
   it "matches 'well, goodbye'" do
-    expect(regex_matching_a_string_ending_with_goodbye).to match "well, goodbye"
+    expect(Anchors.new.ends_with_goodbye).to match "well, goodbye"
   end
 
   it "does not match 'goodbye to  you'" do
-    expect(regex_matching_a_string_ending_with_goodbye).not_to match "goodbye to you"
+    expect(Anchors.new.ends_with_goodbye).not_to match "goodbye to you"
   end
 
-  it "does not match 'goodbye\nfor now'" do
-    expect(regex_matching_a_string_ending_with_goodbye).not_to match "goodbye\nfor now"
+  it 'does not match "goodbye\nfor now"' do
+    expect(Anchors.new.ends_with_goodbye).not_to match "goodbye\nfor now"
   end
 end
 
-describe "regex_matching_greeting_exactly" do
+describe "matching_greeting_exactly" do
   it "matches 'greeting'" do
-    expect(regex_matching_greeting_exactly).to match "greeting"
+    expect(Anchors.new.matching_greeting_exactly).to match "greeting"
   end
 
   it "does not match 'greetings'" do
-    expect(regex_matching_greeting_exactly).not_to match "greetings"
+    expect(Anchors.new.matching_greeting_exactly).not_to match "greetings"
   end
 
   it "does not match 'a greeting'" do
-    expect(regex_matching_greeting_exactly).not_to match "a greeting"
+    expect(Anchors.new.matching_greeting_exactly).not_to match "a greeting"
   end
 
-  it "does not match 'a\ngreeting'" do
-    expect(regex_matching_greeting_exactly).not_to match "a\ngreeting"
+  it 'does not match "a\ngreeting"' do
+    expect(Anchors.new.matching_greeting_exactly).not_to match "a\ngreeting"
   end
 end
 
-describe "regex_matching_exactly_one_word" do
+describe "matching_exactly_one_word" do
   it "matches 'hello'" do
-    expect(regex_matching_exactly_one_word).to match "hello"
+    expect(Anchors.new.matching_exactly_one_word).to match "hello"
   end
 
   it "matches 'A1foo_username'" do
-    expect(regex_matching_exactly_one_word).to match "A1foo_username"
+    expect(Anchors.new.matching_exactly_one_word).to match "A1foo_username"
   end
 
   it "does not match 'hello there'" do
-    expect(regex_matching_exactly_one_word).not_to match "hello there"
+    expect(Anchors.new.matching_exactly_one_word).not_to match "hello there"
   end
 
-  it "does not match 'hello\nthere'" do
-    expect(regex_matching_exactly_one_word).not_to match "hello\nthere"
+  it 'does not match "hello\nthere"' do
+    expect(Anchors.new.matching_exactly_one_word).not_to match "hello\nthere"
   end
 end
 
-describe "regex_matching_the_word_and" do
+describe "matching_the_word_and" do
   it "matches 'and'" do
-    expect(regex_matching_the_word_and).to match "and"
+    expect(Anchors.new.matching_the_word_and).to match "and"
+  end
+
+  it "matches 'and'" do
+    expect(Anchors.new.matching_the_word_and).to match "one and two"
   end
 
   it "does not match 'demand'" do
-    expect(regex_matching_the_word_and).not_to match "demand"
+    expect(Anchors.new.matching_the_word_and).not_to match "i demand to see the papers"
   end
 end
