7ec9af80cde82a6676231c5b6075c79ca3674c18
Gabe Berke-Williams <gabebw@gabebw.com>
2016-04-19 13:11:05 -0700

    Solve it

--- lib/anchors.rb
+++ lib/anchors.rb
@@ -3,33 +3,33 @@ class Anchors
   # Returns a regex that matches a string that has at least one line that has
   # exactly 1 word in it. It may have other lines that don't match this regex.
   def matching_at_least_one_line_that_has_exactly_one_word_in_it
-    /WRITE ME/
+    /^\w+$/
   end
 
   # Returns a regex that matches a string that starts with "hello".
   def matching_a_string_starting_with_hello
-    /WRITE ME/
+    /\Ahello/
   end
 
   # Returns a regex that matches a string that ends with "goodbye".
   def ends_with_goodbye
-    /WRITE ME/
+    /goodbye\z/
   end
 
   # Returns a regex that matches the word "greeting" exactly. It should not
   # match any other words.
   def matching_greeting_exactly
-    /WRITE ME/
+    /\Agreeting\z/
   end
 
   # Returns a regex that matches exactly one word.
   def matching_exactly_one_word
-    /WRITE ME/
+    /\A\w+\z/
   end
 
   # Returns a regex that has the word "and" somewhere in it. This should match
   # "and", but not "demand", for example.
   def matching_the_word_and
-    /WRITE ME/
+    /\band\b/
   end
 end
